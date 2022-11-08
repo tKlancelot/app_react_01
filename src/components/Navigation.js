3baf08e46541738d5dfc811a4cd4d43419b39e80
tKlancelot <princesuper42@gmail.com>
2022-11-08 02:40:43 +0100

    mise en place de la structure sass + ajout logique navigation
    is-active ok

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,11 +4,17 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <div className="navigation">
-      <ul>
-        <NavLink to="/">
+      <ul className="navigation__body">
+        <NavLink
+          to="/"
+          className={(nav) => (nav.isActive ? "link-active" : "")}
+        >
           <li>accueil</li>
         </NavLink>
-        <NavLink to="/about">
+        <NavLink
+          to="/about"
+          className={(nav) => (nav.isActive ? "link-active" : "")}
+        >
           <li>about</li>
         </NavLink>
       </ul>
