ec22423732a39ab5c5e83dd9340d9d4cfbbf6303
tKlancelot <princesuper42@gmail.com>
2022-11-08 02:17:50 +0100

    configuration projet
    settings
    vscode
    sass react router dom
    routing -> routes
    structure sass

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Page About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore
+        tenetur, perspiciatis, omnis impedit exercitationem doloremque quae sint
+        consequatur commodi molestias id fugiat ea, nemo sequi. Atque sed
+        distinctio quisquam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <Navigation />
+        <h1>Accueil</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
